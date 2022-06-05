aea5823b9ae6d3a5b91f7c007be8ba69187a54c5
Francisco de Assis <a124fr@gmail.com>
2022-06-04 21:29:08 -0300

    Criado e mapeado entidade que representa a Vaga de Estacionamento e configurada com jpa

--- src/main/java/com/api/parkcontrol/models/ParkingSpot.java
+++ src/main/java/com/api/parkcontrol/models/ParkingSpot.java
@@ -1,14 +1,123 @@
 package com.api.parkcontrol.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "parking_spot")
 public class ParkingSpot implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String numero;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false,  unique = true, length = 10)
+    private String parkSpotNumber; // numero da vaga do estacionamento
+
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar; // placa do carro
+
+    @Column(nullable = false, length = 70)
+    private String brandCar; // marca do carro
+
+    @Column(nullable = false, length = 70)
+    private String modelCar; // modelo do carro
+
+    @Column(nullable = false, length = 70)
+    private String colorCar; // cor do carro
+
+    @Column(nullable = false)
+    private LocalDateTime registrationDate; // data de registro da vaga
+
+    @Column(nullable = false, length = 130)
+    private String responsibleName; // nome do responsável do veiculo ou apartamento
+
+    @Column(nullable = false, length = 30)
+    private String apartment; // o apartamento
+
+    @Column(nullable = false, length = 30)
+    private String block; // bloco ou torre do condomínio
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getParkSpotNumber() {
+        return parkSpotNumber;
+    }
+
+    public void setParkSpotNumber(String parkSpotNumber) {
+        this.parkSpotNumber = parkSpotNumber;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCar(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCar() {
+        return modelCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public String getColorCar() {
+        return colorCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getResponsibleName() {
+        return responsibleName;
+    }
+
+    public void setResponsibleName(String responsibleName) {
+        this.responsibleName = responsibleName;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
 }
