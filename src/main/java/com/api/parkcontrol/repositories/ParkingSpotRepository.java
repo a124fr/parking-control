1eac3278adf433d1bb1b0d5fb53d21cb2922c128
Francisco de Assis <a124fr@gmail.com>
2022-06-04 21:34:44 -0300

    Anotando o Repository de ParkingSpot

--- src/main/java/com/api/parkcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkcontrol/repositories/ParkingSpotRepository.java
@@ -2,9 +2,11 @@ package com.api.parkcontrol.repositories;
 
 import com.api.parkcontrol.models.ParkingSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, UUID> {
 
 }
